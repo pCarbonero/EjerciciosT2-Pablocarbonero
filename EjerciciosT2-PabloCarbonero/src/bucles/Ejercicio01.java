d78b4b0cc1bfe8af32a78a58407a098ddcfc1772
pCarbonero <pablo.carbonero@iesnervion.es>
2023-11-08 10:16:27 +0100

    [UPD] try catch ej 1 y comienzo ej 3 (bucles)

--- EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio01.java
+++ EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio01.java
@@ -1,57 +1,93 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
 		// variable para las horas, minutos y segundos
-		int hour, min, sec;
+		int hour = -1, min = -1, sec = -1;
 		//variable para el incremento de segundos
-		int inc;
+		int inc = -1;
 		//variable escaner
 		Scanner sc = new Scanner(System.in);
 		
-		//preguntamos por los minutos y segundos y guardamos los valores en cad variable
-		System.out.println("Dime las horas ");
-		hour = sc.nextInt();//guardamos el valor introducido en la variable
-		
-		System.out.println("Dime los minutos ");
-		min = sc.nextInt();//guardamos el valor introducido en la variable
-		
-		System.out.println("Dime los segundos ");
-		sec = sc.nextInt();//guardamos el valor introducido en la variable
-		
-		System.out.println("Dime cuantos segundos quieres incrementar la hora ");
-		inc = sc.nextInt();//guardamos el valor introducido en la variable
-		
-		//comprobamos que la hora están bien introducidas
-		if (hour >= 0 && hour <= 23 && min >= 0 && min <= 59 && sec >= 0 && sec <= 59) {
-			//el bucle se va a repetir tantas veces se como segundos queremos incrementar
-			for (int i = 1; i <= inc; i++) {
-				//en cada incremento suma un segundo
-				sec++;
-				//si los segundos son 60 entonces suma un minuto y los segundos vuelven a 0
-				if (sec == 60) {
-					sec = 00;
-					min += 1;
+		//bucle para asegurarnos de que la hora está bien introducida
+			do {
+				try {
+				//preguntamos por las horas
+				System.out.println("Dime las horas ");
+				hour = sc.nextInt();//guardamos el valor introducido en la variable
+						
+				}catch (InputMismatchException e) {
+					System.out.println("Asegurate de introducir carácteres numéricos");
+					sc.nextLine();
 				}
-				//si los minutos son 60 entonces suma una hora y los minutos vuelven a 0
-				if (min == 60) {
-					min = 00;
-					hour += 1;
+					
+			} while (hour < 0 && hour < 23);
+			
+			do {
+				try {
+				//preguntamos por los minutos y segundos y guardamos los valores en cad variable
+				System.out.println("Dime los minutos ");
+				min = sc.nextInt();//guardamos el valor introducido en la variable
+						
+				}catch (InputMismatchException e) {
+					System.out.println("Asegurate de introducir carácteres numéricos");
+					sc.nextLine();
 				}
-				//si las horas son 24 entonces las horas vuelven a 0
-				if (hour == 24) {
-					hour = 00;
+					
+			} while (min < 0 && min < 60);
+			
+			do {
+				try {
+				//preguntamos por los minutos y segundos y guardamos los valores en cad variable
+				System.out.println("Dime los segundos ");
+				sec = sc.nextInt();//guardamos el valor introducido en la variable
+						
+				}catch (InputMismatchException e) {
+					System.out.println("Asegurate de introducir carácteres numéricos");
+					sc.nextLine();
 				}
-				
+					
+			} while (min < 0 && min < 60);
+		
+			do {
+				try {
+					System.out.println("Dime cuantos segundos quieres incrementar la hora ");
+					inc = sc.nextInt();//guardamos el valor introducido en la variable
+						
+				}catch (InputMismatchException e) {
+					System.out.println("Asegurate de introducir carácteres numéricos");
+					sc.nextLine();
+				}
+					
+			} while (inc < 0);
+	
+
+		//el bucle se va a repetir tantas veces se como segundos queremos incrementar
+		for (int i = 1; i <= inc; i++) {
+			//en cada incremento suma un segundo
+			sec++;
+			//si los segundos son 60 entonces suma un minuto y los segundos vuelven a 0
+			if (sec == 60) {
+				sec = 00;
+				min += 1;
 			}
-		}
-		//si no está bien entonces muestra el mensaje de que hay un error
-		else {
-			System.out.println("Asegurate de que has introducido la hora en rangos válidos ");
-		}
+			//si los minutos son 60 entonces suma una hora y los minutos vuelven a 0
+			if (min == 60) {
+				min = 00;
+				hour += 1;
+			}
+			//si las horas son 24 entonces las horas vuelven a 0
+			if (hour == 24) {
+				hour = 00;
+			}
+			
+		 }
+		
+
 		
 		//imprimimos por la consola la hora tras el incremento
 		System.out.println("La hora es " + hour + ":" + min + ":" + sec);
--- EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio03.java
+++ EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio03.java
@@ -1,11 +1,45 @@
 package bucles;
 
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
+		//variable para la pirámide
+		int num = -1;
+		//escaner
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//try catch
+		do {
+			try {
+				//pedimos el número de la base
+				System.out.println("Dime el número de la base del triángulo ");
+				num = sc.nextInt();			
+			} catch (InputMismatchException e) {
+				System.out.println("Asegurate de que estás introduciendo carácteres numéricos ");
+				sc.nextLine();
+			}//fin catch
+		} while (num < 0);
+		
+		
+		for (int i = 1; i <= num; i++) {
+			for (int j = 1; j <= num - i; j++) {
+				System.out.println(" ");
+				
+			}//fin bucle j
+			System.out.println("* ");
+			sc.nextLine();
+			//cos
+			
+		}//fin bucle i
+		
+		
+		
+	    }//fin main
 
-	    }
-
-	}
+	}//fin class
 
