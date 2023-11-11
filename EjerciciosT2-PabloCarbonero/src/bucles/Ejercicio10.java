c26ccacb732f77f0a8fa17ddc3236f4740d9ba85
Propietario <Propietario@DESKTOP-I8MN8QL>
2023-11-11 13:26:33 +0100

    [UPD] comentarios y pruebas ej 10 bucles

--- EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio10.java
+++ EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio10.java
@@ -4,7 +4,16 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio10 {
-
+	/*
+	 * ENTRADA: -7 | REL. ESPERADO: ERROR | REL. OBTENIDO: ERROR
+	 * ENTRADA: 2 | REL. ESPERADO: CAPICÚA | REL. OBTENIDO: CAPICÚA
+	 * ENTRADA: 43 | REL. ESPERADO: NO CAPICUA | REL. OBTENIDO: NO CAPICUA 
+	 * ENTRADA: 44 | REL. ESPERADO: CAPICÚA | REL. OBTENIDO: CAPICÚA
+	 * ENTRADA: 321 | REL. ESPERADO: NO CAPICUA  | REL. OBTENIDO: NO CAPICUA 
+	 * ENTRADA: 121 | REL. ESPERADO: CAPICÚA | REL. OBTENIDO: CAPICÚA
+	 * ENTRADA: 3217 | REL. ESPERADO: NO CAPICUA  | REL. OBTENIDO: NO CAPICUA 
+	 * ENTRADA: 1221 | REL. ESPERADO: CAPICÚA | REL. OBTENIDO: CAPICÚA
+	 */
 	public static void main(String[] args) {
 		//creamos variable para guaradar el número
 		int num = -1, aux;
@@ -22,27 +31,35 @@ public class Ejercicio10 {
 				//guardamos el número en la variable num
 				num = sc.nextInt();
 			} catch (InputMismatchException e) {
-				System.out.println("Asegurate de introducir carácteres numéricos ");
+				System.out.println("El valor introducido no es correcto ");
 				//"limpiamos" el escaner para introducir de nuevo por consola algo
 				sc.nextLine();
 			}
 		} while (num < 0);
 		
+		//igualamos el número auxiliar al numero para no modificar directamente el número inicial
 		aux = num;
 		
+		//bucle que calculará el número inverso
 		while (aux > 0) {
+			//hacemos el modulo de aux/10 para averiguar la ultima cifra
 			last = aux%10;
+			//multiplicamos la variable *10 y le sumamos el ultimo numero para poco a poco ir montandoi el numero inverso
 			inv = inv * 10 + last;
+			//dividimos entre diez para eliminar la ultima cifra
 			aux/=10;
 		}
 		
+		//si el numeor inverso es igual al inicial entonces es capicua
 		if (inv == num) {
 			System.out.println("CApicua");
+			//si no no
 		}else {
 			System.out.println("NO CApicua");
 		}
 		
-
+		//cerramos el escaner
+		sc.close();
 	}//fin main
 
 }//fin class
