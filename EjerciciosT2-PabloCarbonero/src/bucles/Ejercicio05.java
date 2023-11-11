2c7f938a1a11b0d8bff73c81fb91b4a69e5fcc1d
Propietario <Propietario@DESKTOP-I8MN8QL>
2023-11-11 12:42:57 +0100

    [UPD] pruebas y try catch ej 5 bucles

--- EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio05.java
+++ EjerciciosT2-PabloCarbonero/src/bucles/Ejercicio05.java
@@ -1,25 +1,42 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+/* ENTRADA: 8, 9 | RES. ESPERADO: 72 | RES. OBTENIDO 72
+ * ENTRADA: 6, 12 | RES. ESPERADO: 12 | RES. OBTENIDO 12
+ * ENTRADA: 8.3 | RES. ESPERADO: ERROR | RES. OBTENIDO ERROR
+ * ENTRADA: numero | RES. ESPERADO: ERROR | RES. OBTENIDO ERROR
+ */
 
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
 		// Variable para guardar los números a y b
-				int a, b;
+				int a = -1, b = -2;
 				//variable para guardar el mcm
 				int mcm = 0;
 				//creamos el escaner
 				Scanner sc = new Scanner(System.in);
 				
-				//preguntamos por los valores de a y b y los guardamos en sus respectivas variables
-				System.out.println("Valor de a ");
-				a = sc.nextInt();
+				//creamos el trycatch
+				do {
+					try {
+						//preguntamos por los valores de a y b y los guardamos en sus respectivas variables
+						System.out.println("Valor de a ");
+						a = sc.nextInt();
+						
+						System.out.println("Valor de b ");
+						b = sc.nextInt();
+					} catch (InputMismatchException e) {
+						System.out.println("El valor introducido no es correcto, introdúcelo otra vez");
+						//limpiamos el escaner para poder almacenar un nuevo input
+						sc.nextLine();
+					}
+					
+				} while (a < 0 || b < 0);
 				
-				System.out.println("Valor de b ");
-				b = sc.nextInt();
-	
-								
+												
 				//creamos un bucle en el cual igualamos i a un ternario que comprueba si b es mayor que a de forma que i es igual al numero mayor de los dos
 				for (int i = (a>b) ? a : b; i >= 1; i++) {					
 					//comprobamos si el modulo de a y b con cada numero es 0, si es asi es que ese es el mcm
